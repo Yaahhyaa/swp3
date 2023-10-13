10912ba6dbcb5da5eaa5c2b0c03db287be171fd3
yaahhyaa <yahay.shekh@student.htldornbirn.at>
2023-10-13 09:35:59 +0200

    korrektur while {}

---
+++ src/at/ysm/basic/Würfelspiel.java
@@ -0,0 +1,42 @@
+package at.ysm.basic;
+
+import java.util.Random;
+
+public class Würfelspiel {
+    public static void main(String[] args) {
+        Random random = new Random();
+        char cAbbruch = 0;
+    do {
+
+        int winsOfPlayer= 0;
+        int winsOfComputer = 0;
+        int amountOfDraws = 0;
+
+
+
+
+    for (int i = 0; i < 6; i++) {
+        int randomPlayer = random.nextInt(6) + 1;
+        int randomComputer = random.nextInt(6) + 1;
+
+        if (randomPlayer > randomComputer) {
+            winsOfPlayer++;
+        } else if (randomComputer > randomPlayer) {
+            winsOfComputer++;
+        } else {
+            amountOfDraws++;
+        }
+
+        System.out.println("Computer:" + randomComputer + " " + "player: " + randomPlayer);
+
+
+    }
+    System.out.println("\n");
+    System.out.println(" Computer gesamt wins:" + winsOfComputer + " \n " + "Player gesamt wins:" + winsOfPlayer + "\n " + "insgesammte Draws:" + amountOfDraws);
+
+    }while (cAbbruch != 'j');
+
+        System.out.println("Spiel Beendet");
+
+    }
+}
