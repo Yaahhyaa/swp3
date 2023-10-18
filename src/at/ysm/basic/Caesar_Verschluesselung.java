ec7db461daad120aca0802b1fc1bbf451a06fb3b
yaahhyaa <yahay.shekh@student.htldornbirn.at>
2023-10-18 09:10:14 +0200

    Start

--- src/at/ysm/basic/Caesar_Verschluesselung.java
+++ src/at/ysm/basic/Caesar_Verschluesselung.java
@@ -5,6 +5,19 @@ import java.util.Scanner;
 
 public class Caesar_Verschluesselung {
     public static void main(String[] args) {
+    String result = encrypt(1, "Hallo");
+        System.out.println(result);
+    }
 
+    public static String encrypt(int shift, String stringToEncrypt){
+        String  encryptedString = "";
+    char[] inputData = stringToEncrypt.toCharArray();
+
+        for (int i = 0; i < inputData.length; i++) {
+            int letter = inputData[i] + shift;
+            encryptedString += Character.toString(letter);
+            
+        }
+        return encryptedString;
     }
 }
