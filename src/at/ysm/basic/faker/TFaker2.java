60c71b90fc30c2dc0894084ce94b15295b46a8e5
yaahhyaa <yahay.shekh@student.htldornbirn.at>
2023-11-20 18:29:50 +0100

    Start

--- src/at/ysm/basic/faker/TFaker2.java
+++ src/at/ysm/basic/faker/TFaker2.java
@@ -6,7 +6,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Locale;
 
-public class TFaker {
+public class TFaker2 {
     public static void main(String[] args) {
 
         Faker faker = new Faker(new Locale("DE-AT"));
@@ -15,13 +15,22 @@ public class TFaker {
             FileWriter myWriter = new FileWriter("insertDaten.txt");
 
             for (int i = 0; i < 100; i++) {
+
+
                 String name = faker.name().fullName(); // Miss Samanta Schmidt
                 String firstName = faker.name().firstName(); // Emory
                 String lastName = faker.name().lastName(); // Barton
-                String email = faker.name().firstName() + " " + faker.name().lastName() + "@faker.at";
-                String streetAddress = faker.address().streetAddress(); // 60018 Sawayn Brooks Suite 449
 
-                String record = "Insert into Autoren (AutorID, Name, Email) Values" +  " " + "('"  + name + "' ,'" + lastName + "' ,'" + email +"');";
+                String comicsID = faker.number().digits(5); // Generiere eine 5-stellige Zahl als ComicID
+                String Titel = faker.book().title(); // Generiere einen Buchtitel
+                String SerieID = faker.number().digits(3); // Generiere eine 3-stellige Zahl als SerieID
+                String AutorID = faker.name().fullName(); // Generiere den Namen eines Autors
+                int Veröffentlichungsjahr = faker.number().numberBetween(1900, 2023); // Generiere ein Veröffentlichungsjahr
+
+                String record = "INSERT INTO Comics (ComicID, Titel, SerieID, AutorID, Veröffentlichungsjahr) VALUES ('" +
+                        comicsID + "', '" + Titel + "', '" + SerieID + "', '" + AutorID + "', " + Veröffentlichungsjahr + ");";
+
+
                 myWriter.write(record + "\n"); // Writing each record to a new line in the file
 
             }
